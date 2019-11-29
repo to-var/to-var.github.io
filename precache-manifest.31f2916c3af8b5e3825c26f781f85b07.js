c6352ad9fa8b4ef7f424d372e1a9244a9d8a5025
Jose Tovar <tmjoseantonio@gmail.com>
2019-11-28 20:00:00 -0500

    Updates

---
+++ precache-manifest.31f2916c3af8b5e3825c26f781f85b07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451011743fde13ed57189810940d0426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1c3fce6bbef96ff85c7",
+    "url": "/static/css/main.2b6280bf.chunk.css"
+  },
+  {
+    "revision": "a8f6c94ffd9d8b83babc",
+    "url": "/static/js/2.fee6ae67.chunk.js"
+  },
+  {
+    "revision": "d1c3fce6bbef96ff85c7",
+    "url": "/static/js/main.e515fd15.chunk.js"
+  },
+  {
+    "revision": "6402a5071608cefc3fee",
+    "url": "/static/js/runtime-main.e18f1992.js"
+  }
+]);
